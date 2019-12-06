f6251d59f8d4c0367eab729228ea9e7bc576ab6b
Milo Stephens <milo.a.stephens@durham.ac.uk>
2019-12-06 10:07:43 +0000

    Can now start server with npm start

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-//initialise app
 const express = require('express');
 const app = express();
 
-//configure app
 app.use(express.static('client'));
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
+
+module.exports = app;
