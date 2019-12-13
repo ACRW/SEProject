a9e499940da0033fc8097c7925e4ef7b5c28c153
Milo Stephens <milo.a.stephens@durham.ac.uk>
2019-12-13 09:32:39 +0000

    added google auth dependency

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ app.use(express.static('client'));
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.post('/tokensignin', async function(req, resp) {
+    const token = req.body.token;
+});
+
 module.exports = app;
