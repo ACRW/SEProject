5d38a9d9536ae1c31d3a3df4c6ef1b2a934bc57f
Milo Stephens <milo.a.stephens@durham.ac.uk>
2019-12-13 09:05:56 +0000

    //

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('client'));
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 
