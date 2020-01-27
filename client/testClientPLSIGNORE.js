f2d0cddb3045b6981f02f83f3f343d42f20d5527
Milo Stephens <milo.a.stephens@durham.ac.uk>
2020-01-27 14:44:39 +0000

    continuing work on sign in page

--- client/testClientPLSIGNORE.js
+++ client/testClientPLSIGNORE.js
@@ -1,4 +1,7 @@
 async function onSignIn(googleUser) {
+    document.getElementById('googleButton').setAttribute('hidden', '');
+    document.getElementById('spinner').removeAttribute('hidden');
+
     const token = googleUser.getAuthResponse().id_token;
 
     let response = await fetch('/customersignin',
@@ -10,20 +13,43 @@ async function onSignIn(googleUser) {
         body: 'token=' + token
     });
 
-    const body = await response.text();
-
     if (!response.ok) {
-
         let auth2 = gapi.auth2.getAuthInstance();
         auth2.signOut();
 
-        alert('pls try again.')
+        document.getElementById('googleButton').removeAttribute('hidden');
+        document.getElementById('spinner').setAttribute('hidden', '');
+
+        alert('Something went wrong! Please try again.');
 
     } else {
         if (response.status == 200) {
-            console.log(body)
+            const body = JSON.parse(await response.text());
+
+            document.getElementById('userName').innerHTML = body['fname'] + ' ' + body['sname'];
+
+            document.getElementById('userMenu').removeAttribute('hidden');
+            document.getElementById('signInCard').setAttribute('hidden', '');
+
         } else if (response.status == 201) {
-            console.log('more action')
+
+            document.getElementById('signInCardBody').setAttribute('hidden', '');
+            document.getElementById('newCustomer').removeAttribute('hidden');
         }
     }
 }
+
+document.getElementById('signOut').addEventListener('click', function() {
+    event.preventDefault();
+
+    let auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut();
+
+    document.getElementById('googleButton').removeAttribute('hidden');
+    document.getElementById('spinner').setAttribute('hidden', '');
+
+    document.getElementById('userMenu').setAttribute('hidden', '');
+    document.getElementById('signInCard').removeAttribute('hidden');
+
+    // need to destroy session
+});
