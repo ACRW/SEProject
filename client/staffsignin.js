d72d9d33113229cbb7d40b7fd207b3fdd4892547
Milo Stephens <milo.a.stephens@durham.ac.uk>
2020-04-06 15:11:28 +0100

    refinements to sign in process

--- client/staffsignin.js
+++ client/staffsignin.js
@@ -1,3 +1,42 @@
+// hide & reset sign in card
+function resetSignInCard() {
+    // hide sign in card
+    document.getElementById('signInCard').setAttribute('hidden', '');
+
+    // reset sign in card
+    document.getElementById('googleButton').removeAttribute('hidden');
+    document.getElementById('spinner').setAttribute('hidden', '');
+}
+
+// display welcome message
+function showWelcomeMessage() {
+    // listen for sign out
+    document.getElementById('signOut').addEventListener('click', signOut);
+
+    // listen for new staff link
+    document.getElementById('newStaffLink').addEventListener('click', newStaff);
+
+    // show user dropdown menu
+    document.getElementById('userMenu').removeAttribute('hidden');
+    // show placeholder content
+    document.getElementById('contentCard').removeAttribute('hidden');
+}
+
+// handle sign in error
+function signInError() {
+    // sign out of Google account
+    let auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut();
+
+    // show Google button
+    document.getElementById('googleButton').removeAttribute('hidden');
+    // hide spinner
+    document.getElementById('spinner').setAttribute('hidden', '');
+
+    // inform user of error
+    alert('Something went wrong! We were unable to sign you in. Please refresh the page and try again.');
+}
+
 // sign staff member out
 async function signOut() {
     // prevent default click event
@@ -58,64 +97,101 @@ async function onSignIn(googleUser) {
     // show spinner
     document.getElementById('spinner').removeAttribute('hidden');
 
-    // get ID token
-    const token = googleUser.getAuthResponse().id_token;
-
     // make call to API
-    let response = await fetch('/staffsignin',
+    let sessionResponse = await fetch('/currentuser',
     {
-        method: "POST",
+        method: "GET",
         headers: {
             "Content-Type": "application/x-www-form-urlencoded"
-        },
-        body: 'token=' + token
+        }
     });
 
-    // response body
-    let body = await response.text();
+    // if session not currently active
+    if (sessionResponse.status == 403) {
+        // get ID token
+        const token = googleUser.getAuthResponse().id_token;
 
-    // reset sign in card
-    document.getElementById('googleButton').removeAttribute('hidden');
-    document.getElementById('spinner').setAttribute('hidden', '');
+        // make call to API
+        let response = await fetch('/staffsignin',
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: 'token=' + token
+        });
 
-    // if successfully signed in
-    if (response.ok) {
-        // hide sign in card
-        document.getElementById('signInCard').setAttribute('hidden', '');
+        // response body
+        let body = await response.text();
 
-        // parse JSON
-        body = JSON.parse(body);
+        // if successfully signed in
+        if (response.ok) {
+            // hide & reset sign in card
+            resetSignInCard();
 
-        // set staff member's name on page
-        document.getElementById('userName').innerHTML = body['fname'] + ' ' + body['sname'];
+            // parse JSON
+            body = JSON.parse(body);
 
-        // listen for sign out
-        document.getElementById('signOut').addEventListener('click', signOut);
+            // set staff member's name on page
+            document.getElementById('userName').innerHTML = body['fname'] + ' ' + body['sname'];
 
-        // listen for new staff link
-        document.getElementById('newStaffLink').addEventListener('click', newStaff);
-
-        // show user dropdown menu
-        document.getElementById('userMenu').removeAttribute('hidden');
-        // show placeholder content
-        document.getElementById('contentCard').removeAttribute('hidden');
-
-    } else {
-        // sign out of Google account
-        let auth2 = gapi.auth2.getAuthInstance();
-        auth2.signOut();
+            // display welcome message
+            showWelcomeMessage();
 
         // if denied permission
-        if (body == '0permission') {
-            // hide sign in card
-            document.getElementById('signInCard').setAttribute('hidden', '');
+        } else if (body == '0permission') {
+            // sign out of Google account
+            let auth2 = gapi.auth2.getAuthInstance();
+            auth2.signOut();
+
+            // hide & reset sign in card
+            resetSignInCard();
 
             // show permission card
             document.getElementById('permissionCard').removeAttribute('hidden');
 
         } else {
-            // inform user of error
-            alert('Something went wrong! We were unable to sign you in. Please refresh the page and try again.');
+            // handle sign in error
+            signInError();
+        }
+
+    } else {
+        // extract session details
+        const sessionDetails = JSON.parse(await sessionResponse.text());
+
+        // if customer session
+        if (sessionDetails.type == 'customer') {
+            // handle sign in error
+            signInError();
+
+        } else {
+            // make call to API
+            let staffResponse = await fetch('/currentusername',
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                }
+            });
+
+            // if successfully fetched staff member's name
+            if (staffResponse.status == 200) {
+                // extract staff member's name
+                const staffMemberDetails = JSON.parse(await staffResponse.text());
+
+                // set customer's name on page
+                document.getElementById('userName').innerHTML = staffMemberDetails[0]['fName'] + ' ' + staffMemberDetails[0]['lName'];
+
+                // hide & reset sign in card
+                resetSignInCard();
+
+                // display welcome message
+                showWelcomeMessage();
+
+            } else {
+                // handle sign in error
+                signInError();
+            }
         }
     }
 }
